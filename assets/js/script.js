459123bc46d92515d0451dff07caaa5de10106e8
Sherwin Abraham <sherwinabraham@gmail.com>
2023-06-19 18:18:37 -0500

    Debugging.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,15 @@
 // in the html.
 var today = dayjs().format('dddd, MMMM D');
 $('#currentDay').text(today);
-$(function () {
+var milTime = dayjs().format('HH');
+$(function compareTime() {
+  if(milTime === (document.getElementById('hour-9').dataset.value)){
+    $('.present')
+  }else if(milTime < (document.getElementById('hour-9').dataset.value)){
+    $('.past')
+  }else
+    $('.future')
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,5 +29,6 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // DONE: Add code to display the current date in the header of the page.
 });
+compareTime();
