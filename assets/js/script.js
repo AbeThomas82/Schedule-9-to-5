2a93f9820991ecdd7169cbf8be05d8dd1371d2d8
Sherwin Abraham <sherwinabraham@gmail.com>
2023-06-20 14:16:45 -0500

    Removed color classes in html.

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,10 @@ $(function compareTime() {
       }else
         return "past"
     }
-  
+  compareTime();
+  console.log(compareTime);
+});
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -34,6 +37,4 @@ $(function compareTime() {
   // attribute of each time-block be used to do this?
   //
   // DONE: Add code to display the current date in the header of the page.
-});
-compareTime();
-console.log(compareTime);
+
